cedb417d1d8df2b6d68023f7b01d34fc92836ec4
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2022-01-04 20:26:31 +0100

    Create script to generate index

---
+++ src/generateIndex.ts
@@ -0,0 +1,17 @@
+import { Song, SongPreview } from "./interfaces";
+import { readdirSync, readFileSync } from "fs";
+
+const SONGS_FOLDER = './public/songs'
+
+const list: SongPreview[] = [];
+
+
+readdirSync(SONGS_FOLDER).forEach(file => {
+    if (file === 'index.json') return;
+
+    const { tracks, ...songData }: Song = JSON.parse(readFileSync(`${SONGS_FOLDER}/${file}`, { encoding: 'utf-8' }));
+
+    list.push({ ...songData, file });
+});
+
+console.log(JSON.stringify(list, undefined, 2));
