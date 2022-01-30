d4ceb2fb264dfd47dd8c31e3d302402f0037d973
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2022-01-30 13:44:41 +0100

    Add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
+import * as Sentry from '@sentry/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import './index.css';
+
+if (process.env.REACT_APP_SENTRY_DSN_URL) {
+    Sentry.init({
+        dsn: process.env.REACT_APP_SENTRY_DSN_URL,
+
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    });
+}
 
 const client = new QueryClient();
 
