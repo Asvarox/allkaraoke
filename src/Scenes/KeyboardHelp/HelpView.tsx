bcd916d9624e3196b1c79c2ec6aa57c9ee99a6d1
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2022-04-07 18:41:36 +0200

    Add option to toggle help

--- src/Scenes/KeyboardHelp/HelpView.tsx
+++ src/Scenes/KeyboardHelp/HelpView.tsx
@@ -1,5 +1,7 @@
 import { ComponentType } from 'react';
+import { useHotkeys } from 'react-hotkeys-hook';
 import styled from 'styled-components';
+import createPersistedState from 'use-persisted-state';
 import { typography } from '../../Elements/cssMixins';
 import { HelpEntry } from './Context';
 
@@ -7,51 +9,54 @@ interface Props {
     help: HelpEntry | undefined;
 }
 
+const useIsVisible = createPersistedState('keyboard-help-visibility');
+
 export default function KeyboardHelpView({ help }: Props) {
+    const [isVisible, setIsVisible] = useIsVisible(true);
+
+    useHotkeys('h', () => setIsVisible(!isVisible), undefined, [isVisible]);
+
     const helps = Object.entries(help ?? {}).filter(([, value]) => value !== undefined);
     return (
         <>
-            {helps.length && (
+            {isVisible && !!helps.length && (
                 <Container>
                     {helps.map(([type, label]) => {
                         const { view: Component, defaultLabel } = KeyhelpComponent[type as keyof HelpEntry];
                         return (
                             <Section key={type}>
+                                <SectionHelp>{label ?? defaultLabel}</SectionHelp>
                                 <SectionKeys>
                                     <Component />
                                 </SectionKeys>
-                                <SectionHelp>{label ?? defaultLabel}</SectionHelp>
                             </Section>
                         );
                     })}
+                    <Section>
+                        <SectionHelp>Show/hide this help</SectionHelp>
+                        <SectionKeys>
+                            <Kbd>H</Kbd>
+                        </SectionKeys>
+                    </Section>
                 </Container>
             )}
         </>
     );
 }
 
-const Horizontal = () => (
+const HorizontalVerticalBase = ({ vertical = false, horizontal = false }) => (
     <>
-        <Kbd>←</Kbd>
-        <Kbd>→</Kbd>
-    </>
-);
-const Vertical = () => (
-    <>
-        <Kbd>↑</Kbd>
+        <Kbd disabled={!vertical}>↑</Kbd>
         <br />
-        <Kbd>↓</Kbd>
+        <Kbd disabled={!horizontal}>←</Kbd>
+        <Kbd disabled={!vertical}>↓</Kbd>
+        <Kbd disabled={!horizontal}>→</Kbd>
     </>
 );
-const HorizontalVertical = () => (
-    <>
-        <Kbd>↑</Kbd>
-        <br />
-        <Kbd>←</Kbd>
-        <Kbd>↓</Kbd>
-        <Kbd>→</Kbd>
-    </>
-);
+
+const Horizontal = () => <HorizontalVerticalBase horizontal />;
+const Vertical = () => <HorizontalVerticalBase vertical />;
+const HorizontalVertical = () => <HorizontalVerticalBase vertical horizontal />;
 const Accept = () => (
     <>
         <Kbd>Enter ⏎</Kbd>
@@ -81,12 +86,13 @@ const Section = styled.div`
 const SectionKeys = styled.div`
     flex-wrap: nowrap;
     text-align: center;
-    flex: 1;
+    flex: 2;
 `;
 const SectionHelp = styled.span`
-    flex: 1;
+    flex: 3;
     ${typography};
     font-size: 20px;
+    text-align: right;
 `;
 
 const Container = styled.div`
@@ -98,10 +104,12 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
-    width: 320px;
+    width: 340px;
+
+    z-index: 100000;
 `;
 
-const Kbd = styled.kbd`
+const Kbd = styled.kbd<{ disabled?: boolean }>`
     margin: 0.1em;
     padding: 0.05em 0.5em;
     border-radius: 3px;
@@ -113,4 +121,6 @@ const Kbd = styled.kbd`
     box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.2), inset 0px 0px 0px 2px #ffffff;
     background-color: rgb(247, 247, 247);
     text-shadow: 0 1px 0 #fff;
+
+    opacity: ${(props) => (props.disabled ? 0.25 : 1)};
 `;
