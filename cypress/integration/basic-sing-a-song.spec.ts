f56641ff39114c41555b91d2d9a2cc693f14ba6e
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2022-04-18 09:25:55 +0200

    Try fixing the test

--- cypress/integration/basic-sing-a-song.spec.ts
+++ cypress/integration/basic-sing-a-song.spec.ts
@@ -13,27 +13,6 @@ describe('Sing a song', () => {
         cy.intercept('GET', '/songs/e2e-test-multitrack.json', { fixture: 'songs/e2e-test-multitrack.json' });
     });
 
-    it('allows song setup using keyboard', () => {
-        cy.get('[data-test="sing-a-song"]').click();
-        cy.wait(500);
-        cy.get('body').type('{enter}'); // enter first song
-        cy.get('body').type('{backspace}'); // enter first song
-        cy.get('body').type('{rightarrow}'); // next song
-        cy.get('body').type('{enter}'); // focus
-        cy.get('body').type('{uparrow}'); // player 2 track
-        cy.get('body').type('{enter}'); // change to track 1
-        cy.get('body').type('{uparrow}'); // player 1 track
-        cy.get('body').type('{enter}'); // change to track 2
-        cy.get('body').type('{uparrow}'); // game mode
-        cy.get('body').type('{enter}'); // change to pass the mic
-        cy.get('body').type('{uparrow}'); // difficulty
-        cy.get('body').type('{enter}'); // change to hard
-        cy.get('[data-test="player-2-track-setting"]').should('have.attr', 'data-test-value', '1');
-        cy.get('[data-test="player-1-track-setting"]').should('have.attr', 'data-test-value', '2');
-        cy.get('[data-test="game-mode-setting"]').should('have.attr', 'data-test-value', 'Pass The Mic');
-        cy.get('[data-test="difficulty-setting"]').should('have.attr', 'data-test-value', 'Hard');
-    });
-
     it('goes through singing properly', () => {
         cy.get('[data-test="sing-a-song"]').click();
         cy.wait(500);
@@ -56,6 +35,27 @@ describe('Sing a song', () => {
 
         cy.get('[data-test="play-next-song-button"]', { timeout: 30_000 }).click();
         cy.get('[data-test="song-e2e-test.json"]').should('exist');
+    });
+
+    it('allows song setup using keyboard', () => {
+        cy.get('[data-test="sing-a-song"]').click();
+        cy.wait(500);
+        cy.get('body').type('{enter}'); // enter first song
+        cy.get('body').type('{backspace}'); // enter first song
+        cy.get('body').type('{rightarrow}'); // next song
+        cy.get('body').type('{enter}'); // focus
+        cy.get('body').type('{uparrow}'); // player 2 track
+        cy.get('body').type('{enter}'); // change to track 1
+        cy.get('body').type('{uparrow}'); // player 1 track
+        cy.get('body').type('{enter}'); // change to track 2
+        cy.get('body').type('{uparrow}'); // game mode
+        cy.get('body').type('{enter}'); // change to pass the mic
+        cy.get('body').type('{uparrow}'); // difficulty
+        cy.get('body').type('{enter}'); // change to hard
+        cy.get('[data-test="player-2-track-setting"]').should('have.attr', 'data-test-value', '1');
+        cy.get('[data-test="player-1-track-setting"]').should('have.attr', 'data-test-value', '2');
+        cy.get('[data-test="game-mode-setting"]').should('have.attr', 'data-test-value', 'Pass The Mic');
+        cy.get('[data-test="difficulty-setting"]').should('have.attr', 'data-test-value', 'Hard');
     });
 
     describe('Filters', () => {
