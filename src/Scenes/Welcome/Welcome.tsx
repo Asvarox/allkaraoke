1d1f18725b35a559b937a2d36be03d83d5e73619
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2022-04-21 22:36:04 +0200

    Add github ribbon

--- src/Scenes/Welcome/Welcome.tsx
+++ src/Scenes/Welcome/Welcome.tsx
@@ -1,5 +1,6 @@
 import { MenuButton, MenuContainer } from 'Elements/Menu';
 import { navigate } from 'Hooks/useHashLocation';
+import GithubRibbon from 'Scenes/Welcome/GithubRibbon';
 import styled from 'styled-components';
 import { Link } from 'wouter';
 import useKeyboard from '../../Hooks/useKeyboard';
@@ -8,41 +9,46 @@ import logo from './logo.gif';
 function Welcome() {
     const { register } = useKeyboard();
     return (
-        <Container>
-            <Logo src={logo} alt="Olkaraoke logo" />
-            <MenuContainer>
-                <Link href="/game">
-                    <MenuButton data-test="sing-a-song" {...register('sing a song', () => navigate('/game'))}>
-                        Sing a song
-                    </MenuButton>
-                </Link>
-                {false && (
-                    <Link href="/select-input">
-                        <MenuButton {...register('select input', () => navigate('/select-input'))}>
-                            Setup Microphones
+        <>
+            <GithubRibbon />
+            <Container>
+                <Logo src={logo} alt="Olkaraoke logo" />
+                <MenuContainer>
+                    <Link href="/game">
+                        <MenuButton data-test="sing-a-song" {...register('sing a song', () => navigate('/game'))}>
+                            Sing a song
                         </MenuButton>
                     </Link>
-                )}
-                <Link href="/jukebox">
-                    <MenuButton data-test="jukebox" {...register('jukebox', () => navigate('/jukebox'))}>
-                        Jukebox
-                    </MenuButton>
-                </Link>
-                {false && (
-                    <Link href="/connect-phone">
-                        <MenuButton {...register('connect phone', () => navigate('/connect-phone'))}>
-                            Connect Phone
+                    {false && (
+                        <Link href="/select-input">
+                            <MenuButton {...register('select input', () => navigate('/select-input'))}>
+                                Setup Microphones
+                            </MenuButton>
+                        </Link>
+                    )}
+                    <Link href="/jukebox">
+                        <MenuButton data-test="jukebox" {...register('jukebox', () => navigate('/jukebox'))}>
+                            Jukebox
                         </MenuButton>
                     </Link>
-                )}
-                <Link href="/edit">
-                    <MenuButton {...register('edit songs', () => navigate('/edit'))}>Edit songs</MenuButton>
-                </Link>
-                <Link href="/convert">
-                    <MenuButton {...register('convert', () => navigate('/convert'))}>Convert UltraStar .txt</MenuButton>
-                </Link>
-            </MenuContainer>
-        </Container>
+                    {false && (
+                        <Link href="/connect-phone">
+                            <MenuButton {...register('connect phone', () => navigate('/connect-phone'))}>
+                                Connect Phone
+                            </MenuButton>
+                        </Link>
+                    )}
+                    <Link href="/edit">
+                        <MenuButton {...register('edit songs', () => navigate('/edit'))}>Edit songs</MenuButton>
+                    </Link>
+                    <Link href="/convert">
+                        <MenuButton {...register('convert', () => navigate('/convert'))}>
+                            Convert UltraStar .txt
+                        </MenuButton>
+                    </Link>
+                </MenuContainer>
+            </Container>
+        </>
     );
 }
 
