4273c24ccab4b417e2253fa8e0ccacad8f2cf070
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2022-10-20 06:00:44 +0200

    fix unit tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     roots: ['<rootDir>/src'],
     collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-    testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+    testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.test.{js,jsx,ts,tsx}'],
     testEnvironment: 'jsdom',
     transform: {
         '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/config/jest/babelTransform.js',
