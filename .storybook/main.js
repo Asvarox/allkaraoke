a45854987a50e01c6c3e0e497bf20890161ca270
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2022-10-22 09:22:17 +0200

    Upgrade to storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,10 +4,11 @@ const react = require('@vitejs/plugin-react').default;
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-vite',
+    framework: {
+        name: '@storybook/react-vite',
+        options: {},
     },
+    core: {},
     features: {
         storyStoreV7: true,
     },
