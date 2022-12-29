efeb0ed6a2355274e30af28f1555bbd8dbbe7367
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2022-12-29 10:47:57 +0100

    Very important commit

--- src/Elements/Logo.tsx
+++ src/Elements/Logo.tsx
@@ -1,7 +1,18 @@
-import logo from 'Elements/logo.gif';
-import styled from '@emotion/styled';
+import logo from "Elements/logo.gif";
+import pride from "Elements/pride.png";
+import styled from "@emotion/styled";
 
 export default function Logo(props: any) {
-    return <StyledLogo src={logo} alt="Olkaraoke logo" {...props} />;
+    return (<Container>
+        {window.location.host === 'konfederacja.gay' && <PrideLogo src={pride} alt="AllKaraoke Pride logo" />}
+        <StyledLogo src={logo} alt="AllKaraoke logo" {...props} />
+
+    </Container>);
 }
 const StyledLogo = styled.img``;
+
+const Container = styled.div`
+    position: relative;
+    `;
+
+const PrideLogo = styled.img` position: absolute;`;
