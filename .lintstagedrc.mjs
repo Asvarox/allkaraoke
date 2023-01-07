9eb1c8791a8e458c37f6e4d20c1289d20a3db03e
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2023-01-07 01:05:31 +0100

    Add Whenever You Will Go

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,5 @@
 export default {
     "*.{mjs,ts,tsx}": ["eslint --cache --fix"],
     "*.{ts,tsx}": () => 'tsc --noEmit',
+    "public/songs/*.json": ["yarn generate-index", "git add public/songs/index.json"],
 }
