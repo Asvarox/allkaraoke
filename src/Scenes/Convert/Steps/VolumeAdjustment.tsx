e4a351020a9672f8b08a02ce2736e344fd1ec072
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2023-04-19 21:14:25 +0200

    Toggle video and reference sounds so it's easier to adjust the volume

--- src/Scenes/Convert/Steps/VolumeAdjustment.tsx
+++ src/Scenes/Convert/Steps/VolumeAdjustment.tsx
@@ -2,7 +2,7 @@ import { Box, Slider } from '@mui/material';
 import YouTube from 'react-youtube';
 import backgroundMusic from 'assets/Funk Cool Groove (No Copyright Music) By Anwar Amr.mp3';
 import { SongMetadataEntity } from 'Scenes/Convert/Steps/SongMetadata';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
     onChange: (data: SongMetadataEntity) => void;
@@ -12,6 +12,7 @@ interface Props {
 
 export default function VolumeAdjustment(props: Props) {
     const player = useRef<YouTube | null>(null);
+    const reference = useRef<HTMLAudioElement | null>(null);
 
     useEffect(() => {
         player.current?.getInternalPlayer().setVolume(props.data.volume * 100);
@@ -31,24 +32,53 @@ export default function VolumeAdjustment(props: Props) {
         };
     }, [props.data, props.onChange, player.current]);
 
+    const [isPlayerPlaying, setIsPlayerPlaying] = useState(false);
+    const [isReferencePlaying, setIsReferencePlaying] = useState(false);
+
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
             <h3>Adjust volume</h3>
             <Box sx={{ display: 'flex', gap: 5 }}>
                 <Box>
                     <h4>Song</h4>
-                    <YouTube videoId={props.videoId} ref={player} />
+                    <YouTube
+                        videoId={props.videoId}
+                        ref={player}
+                        onPlay={() => {
+                            setIsPlayerPlaying(true);
+
+                            if (isReferencePlaying) {
+                                reference.current?.pause();
+                                setTimeout(() => setIsReferencePlaying(true), 200);
+                            }
+                        }}
+                        onPause={() => {
+                            setIsReferencePlaying(false);
+                        }}
+                    />
                 </Box>
                 <Box>
                     <h4>Reference sound</h4>
                     <p>Use the slider below to make the video volume roughly the same as this music.</p>
                     <br />
                     <audio
+                        ref={reference}
                         controls
                         src={backgroundMusic}
                         loop
                         onPlay={(e) => {
                             e.currentTarget.volume = 0.5;
+                            setIsReferencePlaying(true);
+                            if (isPlayerPlaying) {
+                                player.current?.getInternalPlayer()?.pauseVideo();
+                                setTimeout(() => setIsPlayerPlaying(true), 200);
+                            }
+                        }}
+                        onPause={() => {
+                            setIsReferencePlaying(false);
+                            if (isPlayerPlaying) {
+                                player.current?.getInternalPlayer()?.playVideo();
+                            }
                         }}
                     />
                 </Box>
