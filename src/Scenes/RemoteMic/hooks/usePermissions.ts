5a34f9c50a28bcb67d9ab79559de6d91917295f5
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2023-07-23 13:55:59 +0200

    Fix tests

--- src/Scenes/RemoteMic/hooks/usePermissions.ts
+++ src/Scenes/RemoteMic/hooks/usePermissions.ts
@@ -2,7 +2,7 @@ import events from 'GameEvents/GameEvents';
 import { useEventListener } from 'GameEvents/hooks';
 
 export default function usePermissions() {
-    const [permissions] = useEventListener(events.remoteMicPermissionsSet) ?? ['write'];
+    const [permissions] = useEventListener(events.remoteMicPermissionsSet, true) ?? ['write'];
 
     return permissions;
 }
