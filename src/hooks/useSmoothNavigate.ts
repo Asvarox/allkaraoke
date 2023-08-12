35bf08b647f22289f4423185dfe044a4abdf627d
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2023-08-12 20:05:06 +0200

    re-enable smooth navigate

--- src/hooks/useSmoothNavigate.ts
+++ src/hooks/useSmoothNavigate.ts
@@ -10,10 +10,7 @@ export default function useSmoothNavigate() {
     const [, baseNavigate] = useLocation();
 
     return (to: string, { smooth = true, ...options }: Options = {}) => {
-        // This disabled smooth navigation with viewTransition as it crashes the app on Chrome
-        // See https://bugs.chromium.org/p/chromium/issues/detail?id=1443134
-        // @ts-ignore
-        if (!window.__enableSmooth || !smooth) {
+        if (!smooth) {
             baseNavigate(to, options);
         } else {
             startViewTransition(() => baseNavigate(to, options));
