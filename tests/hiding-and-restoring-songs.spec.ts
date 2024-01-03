5813330ae976878ec6ef35ce3d48a43ed2f52ada
Renata <renbabiuch@gmail.com>
2024-01-03 20:36:14 +0100

    move songID to a variable

--- tests/hiding-and-restoring-songs.spec.ts
+++ tests/hiding-and-restoring-songs.spec.ts
@@ -10,7 +10,8 @@ test.beforeEach(async ({ page, context, browser }) => {
   await mockSongs({ page, context });
 });
 
-const songName: string = 'New Christmas';
+const songID = 'e2e-christmas-english-1995';
+const songName = 'New Christmas';
 
 test('Hiding and restoring songs works', async ({ page }) => {
   await page.goto('/?e2e-test');
@@ -21,21 +22,21 @@ test('Hiding and restoring songs works', async ({ page }) => {
   await page.getByTestId('manage-songs').click();
   await page.getByTestId('edit-songs').click();
   await page.locator("input[placeholder='Search']").fill(songName);
-  await page.locator("button[data-test='hide-song'][data-song='e2e-christmas-english-1995']").click();
+  await page.locator(`button[data-test='hide-song'][data-song='${songID}']`).click();
   await page.getByTestId('main-menu-link').click();
   await page.getByTestId('sing-a-song').click();
   await page.getByTestId('close-exclude-languages').click();
 
-  await expect(page.getByTestId('song-e2e-christmas-english-1995')).not.toBeVisible();
+  await expect(page.getByTestId(`song-${songID}`)).not.toBeVisible();
 
   await page.goBack();
   await page.goBack();
   await page.getByTestId('manage-songs').click();
   await page.getByTestId('edit-songs').click();
   await page.locator("input[placeholder='Search']").fill(songName);
-  await page.locator("button[data-test='restore-song'][data-song='e2e-christmas-english-1995']").click();
+  await page.locator(`button[data-test='restore-song'][data-song='${songID}']`).click();
   await page.getByTestId('main-menu-link').click();
   await page.getByTestId('sing-a-song').click();
 
-  await expect(page.getByTestId('song-e2e-christmas-english-1995')).toBeVisible();
+  await expect(page.getByTestId(`song-${songID}`)).toBeVisible();
 });
