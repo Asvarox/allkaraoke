1c4917dbb48065794c85723f37ece353611a423f
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2024-01-07 17:44:36 +0100

    show dummy high scores to fill the table, addresses #81

--- src/Scenes/Game/Singing/PostGame/PostGame.tsx
+++ src/Scenes/Game/Singing/PostGame/PostGame.tsx
@@ -1,7 +1,7 @@
 import PlayersManager from 'Players/PlayersManager';
 import PostGameView from 'Scenes/Game/Singing/PostGame/PostGameView';
-import { useSongStats } from 'Songs/stats/hooks';
-import { HighScoreEntity, SingSetup, Song } from 'interfaces';
+import useHighScores from 'Scenes/Game/Singing/PostGame/hooks/useHighScores';
+import { SingSetup, Song } from 'interfaces';
 import { useMemo } from 'react';
 import GameState from '../GameState/GameState';
 
@@ -11,29 +11,6 @@ interface Props {
   song: Song;
   onClickSongSelection: () => void;
   singSetup: SingSetup;
-}
-
-function useHighScores(song: Song, singSetup: SingSetup): HighScoreEntity[] {
-  const stats = useSongStats(song);
-
-  const highScores = useMemo(
-    () =>
-      stats?.scores
-        .filter(({ setup }) => setup.mode === singSetup.mode && setup.tolerance === singSetup.tolerance)
-        .map((score) =>
-          score.scores.map((singleScore) => ({
-            ...singleScore,
-            date: score.date,
-            singSetupId: score.setup.id,
-          })),
-        )
-        .flat()
-        .sort((a, b) => b.score - a.score)
-        .slice(0, 5),
-    [stats, singSetup],
-  );
-
-  return highScores ?? [];
 }
 
 function PostGame({ song, width, height, onClickSongSelection, singSetup }: Props) {
---
+++ src/Scenes/Game/Singing/PostGame/hooks/useHighScores.ts
@@ -0,0 +1,68 @@
+import { useSongStats } from 'Songs/stats/hooks';
+import { HighScoreEntity, SingSetup, Song } from 'interfaces';
+import { useMemo } from 'react';
+import isE2E from 'utils/isE2E';
+import seededRandom from 'utils/seededRandom';
+
+const fakePlayerNames = [
+  'Lysa Arryn',
+  'Peter Parker',
+  'Zordon',
+  'Chad Khalimov',
+  'Dat Boi',
+  'Good Guy Greg',
+  'Scumbag Steve',
+  'Meme Man',
+  'Pepe',
+  'Wojak',
+  'Harambe',
+  'Shrek',
+  'Beat of Vadovitz',
+  'Krasnolud',
+  'Mały Aragorn',
+  'Morning Breeze',
+  'Auytjak Lorav',
+  'Smelly Cat',
+  'Stinky Foot',
+];
+const fakeDateFrom = new Date('2016-01-01').getTime();
+const fakeDateTo = new Date('2024-01-07').getTime();
+
+const MAX_SCORES = 5;
+
+export default function useHighScores(song: Song, singSetup: SingSetup): HighScoreEntity[] {
+  const stats = useSongStats(song);
+
+  const highScores = useMemo(() => {
+    const fakeNamesPool = [...fakePlayerNames];
+    const fakeScores: HighScoreEntity[] = Array.from({ length: MAX_SCORES }, (_, i) => {
+      const random = seededRandom(song.id + i);
+      const score = 600_000 + (1_400_000 / (MAX_SCORES - 1)) * i;
+      const name = fakeNamesPool.splice(Math.floor(random * fakeNamesPool.length), 1)[0];
+      return {
+        singSetupId: i.toString(),
+        name: name,
+        score: isE2E() ? score / 1000 : score,
+        date: new Date((fakeDateTo - fakeDateFrom) * random + fakeDateFrom).toISOString(),
+      };
+    });
+
+    return [
+      ...fakeScores,
+      ...(stats?.scores
+        .filter(({ setup }) => setup.mode === singSetup.mode && setup.tolerance === singSetup.tolerance)
+        .map((score) =>
+          score.scores.map((singleScore) => ({
+            ...singleScore,
+            date: score.date,
+            singSetupId: score.setup.id,
+          })),
+        )
+        .flat() ?? []),
+    ]
+      .sort((a, b) => b.score - a.score)
+      .slice(0, MAX_SCORES);
+  }, [song.id, stats, singSetup]);
+
+  return highScores ?? [];
+}
