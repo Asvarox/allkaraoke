668f27d1a6e42060f2d57c146226b60b1c6eb993
Renata <renbabiuch@gmail.com>
2024-01-09 20:46:01 +0100

    refactor Page Object

--- tests/PageObjects/GamePage.ts
+++ tests/PageObjects/GamePage.ts
@@ -9,4 +9,24 @@ export class GamePagePO {
     await this.page.waitForTimeout(1500);
     await this.page.keyboard.press('Enter');
   }
+
+  public async restartSong() {
+    await this.page.getByTestId('button-restart-song').click();
+  }
+
+  public get playersScoreElement() {
+    return this.page.getByTestId('players-score');
+  }
+
+  public get currentPlayersScore() {
+    return this.playersScoreElement.getAttribute('data-score');
+  }
+
+  public async waitForPlayersScoreToBeGreaterThan() {
+    await expect(async () => {
+      const p1score = await this.currentPlayersScore;
+
+      expect(parseInt(p1score!, 10)).toBeGreaterThan(100);
+    }).toPass({ timeout: 15_000 });
+  }
 }
