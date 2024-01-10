6facc4f3695548e29e3acb2e9409326f4db48784
Renata <renbabiuch@gmail.com>
2024-01-10 20:32:48 +0100

    rewrite exclude-languages-test including Page Objects

--- tests/exclude-languages.spec.ts
+++ tests/exclude-languages.spec.ts
@@ -10,63 +10,59 @@ test.beforeEach(async ({ page, context, browser }) => {
   await mockSongs({ page, context });
 });
 
+const language1 = 'Polish';
+const language2 = 'English';
+const song1 = 'e2e-single-english-1995';
+const song2 = 'e2e-english-polish-1994';
+const song3 = 'e2e-multitrack-polish-1994';
+
 test('exclude languages from first start and menu', async ({ page }) => {
   await page.goto('/?e2e-test');
   await pages.landingPage.enterTheGame();
-  await page.getByTestId('advanced').click();
-  await page.getByTestId('save-button').click();
+  await page.pause();
+  await pages.inputSelectionPage.selectAdvancedSetup();
+  await pages.advancedConnectionPage.saveAndGoToSing();
 
   await test.step('Exclude Polish', async () => {
-    await page.getByTestId('sing-a-song').click();
-    await page.getByTestId('lang-Polish').click();
-    await expect(page.getByTestId('lang-Polish')).toBeVisible();
-    await page.getByTestId('close-exclude-languages').click();
-
-    await expect(page.getByTestId('song-e2e-single-english-1995')).toBeVisible();
-    await expect(page.getByTestId('song-e2e-english-polish-1994')).toBeVisible();
-    await expect(page.getByTestId('song-e2e-multitrack-polish-1994')).not.toBeVisible();
+    await pages.mainMenuPage.goToSingSong();
+    await pages.songLanguagesPage.unselectLanguage(language1); // turn off 'Polish'!
+    await pages.songLanguagesPage.continueAndGoToSongList();
+    await expect(pages.songListPage.getSongElement(song1)).toBeVisible();
+    await expect(pages.songListPage.getSongElement(song2)).toBeVisible();
+    await expect(pages.songListPage.getSongElement(song3)).not.toBeVisible();
   });
 
   await test.step('Exclude English', async () => {
     await page.keyboard.press('Backspace'); // Main menu
-
-    await page.getByTestId('manage-songs').click();
-    await page.getByTestId('exclude-languages').click();
-    await page.getByTestId('lang-Polish').click();
-    await page.getByTestId('lang-English').click();
-    await expect(page.getByTestId('lang-Polish')).toBeVisible();
-    await page.getByTestId('close-exclude-languages').click();
-
-    await page.getByTestId('sing-a-song').click();
-
-    await expect(page.getByTestId('song-e2e-english-polish-1994')).toBeVisible();
-    await expect(page.getByTestId('song-e2e-multitrack-polish-1994')).toBeVisible();
-    await expect(page.getByTestId('song-e2e-single-english-1995')).not.toBeVisible();
+    await pages.mainMenuPage.goToManageSongs();
+    await pages.manageSongsPage.goToSelectSongLanguage();
+    await pages.songLanguagesPage.selectLanguage(language1);
+    await pages.songLanguagesPage.unselectLanguage(language2);
+    await pages.songLanguagesPage.continueAndGoToSongList();
+    await pages.mainMenuPage.goToSingSong();
+    await expect(pages.songListPage.getSongElement(song2)).toBeVisible();
+    await expect(pages.songListPage.getSongElement(song3)).toBeVisible();
+    await expect(pages.songListPage.getSongElement(song1)).not.toBeVisible();
   });
 
   await test.step('Include all', async () => {
     await page.keyboard.press('Backspace'); // Main menu
-
-    await page.getByTestId('manage-songs').click();
-    await page.getByTestId('exclude-languages').click();
-    await page.getByTestId('lang-English').click();
-    await expect(page.getByTestId('lang-Polish')).toBeVisible();
-    await page.getByTestId('close-exclude-languages').click();
-
-    await page.getByTestId('sing-a-song').click();
-
-    await expect(page.getByTestId('song-e2e-single-english-1995')).toBeVisible();
-    await expect(page.getByTestId('song-e2e-multitrack-polish-1994')).toBeVisible();
-    await expect(page.getByTestId('song-e2e-english-polish-1994')).toBeVisible();
+    await pages.mainMenuPage.goToManageSongs();
+    await pages.manageSongsPage.goToSelectSongLanguage();
+    await pages.songLanguagesPage.selectLanguage(language2);
+    await pages.songLanguagesPage.continueAndGoToSongList();
+    await pages.mainMenuPage.goToSingSong();
+    await expect(pages.songListPage.getSongElement(song1)).toBeVisible();
+    await expect(pages.songListPage.getSongElement(song3)).toBeVisible();
+    await expect(pages.songListPage.getSongElement(song2)).toBeVisible();
   });
 
   await test.step('exclude all', async () => {
     await page.keyboard.press('Backspace'); // Main menu
-
-    await page.getByTestId('manage-songs').click();
-    await page.getByTestId('exclude-languages').click();
-    await page.getByTestId('lang-English').click();
-    await page.getByTestId('lang-Polish').click();
-    await expect(page.getByTestId('all-languages-excluded-warning')).toBeVisible();
+    await pages.mainMenuPage.goToManageSongs();
+    await pages.manageSongsPage.goToSelectSongLanguage();
+    await pages.songLanguagesPage.unselectLanguage(language2);
+    await pages.songLanguagesPage.unselectLanguage(language1);
+    await expect(pages.songLanguagesPage.allLanguagesExcludedAlert).toBeVisible();
   });
 });
