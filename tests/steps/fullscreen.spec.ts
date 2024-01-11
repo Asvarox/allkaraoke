9284ad23478db15d657d827676f8aab8285bba6a
Renata <renbabiuch@gmail.com>
2024-01-11 19:26:28 +0100

    add fullscreen-test

---
+++ tests/steps/fullscreen.spec.ts
@@ -0,0 +1,26 @@
+import { test } from '@playwright/test';
+
+import initialise from '../PageObjects/initialise';
+import { initTestMode, mockSongs } from '../helpers';
+
+let pages: ReturnType<typeof initialise>;
+test.beforeEach(async ({ page, context, browser }) => {
+  pages = initialise(page, context, browser);
+  await initTestMode({ page, context });
+  await mockSongs({ page, context });
+});
+
+test('Fullscreen is turning on automatically, if user doesnt turn off fullscreen before', async ({ page }) => {
+  await page.goto('/?e2e-test');
+  await pages.landingPage.enterTheGame();
+  await pages.inputSelectionPage.skipToMainMenu();
+  await pages.mainMenuPage.expectFullscreenIsOff();
+  await pages.mainMenuPage.goToSingSong();
+  await pages.songLanguagesPage.continueAndGoToSongList();
+  await pages.songListPage.expectFullscreenIsOn();
+  await page.keyboard.press('Backspace');
+  await pages.mainMenuPage.toggleFullscreen();
+  await pages.mainMenuPage.expectFullscreenIsOff();
+  await pages.mainMenuPage.goToSingSong();
+  await pages.songListPage.expectFullscreenIsOff();
+});
