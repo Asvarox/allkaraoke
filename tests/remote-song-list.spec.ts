3fc34096befa421c01660116af5a89936d7027a4
Renata <renbabiuch@gmail.com>
2024-01-12 19:19:54 +0100

    rewrite remote-song-list-test including Page Objects

--- tests/remote-song-list.spec.ts
+++ tests/remote-song-list.spec.ts
@@ -17,46 +17,50 @@ test.beforeEach(async ({ page, context, browser }) => {
 test.use({ serviceWorkers: 'block' });
 
 const P1_Name = 'E2E Test Blue';
+const songName = 'Last song';
+const songID = 'zzz-last-polish-1994';
+const videoURL = 'https://www.youtube.com/watch?v=8YKAHgwLEMg';
+const convertedSongName = 'test';
+const convertedSongID = 'convert-test';
 
 test('Remote mic song list', async ({ page, context, browserName }) => {
   test.fixme(browserName === 'firefox', 'Test fails super often on FF');
   test.slow();
   await page.goto('/?e2e-test');
   await pages.landingPage.enterTheGame();
-  await page.getByTestId('remote-mics').click();
+  await pages.inputSelectionPage.selectSmartphones();
 
   const remoteMic = await openRemoteMic(page, context);
   await remoteMic.getByTestId('player-name-input').fill(P1_Name);
 
   await test.step('Song list is available without connecting', async () => {
     await remoteMic.getByTestId('menu-song-list').click();
-
-    await expect(await remoteMic.getByTestId('zzz-last-polish-1994')).toBeVisible();
+    await expect(remoteMic.getByTestId(songID)).toBeVisible();
   });
+
   await test.step('Song list doesnt contain removed songs after connecting', async () => {
     await remoteMic.getByTestId('menu-microphone').click();
     await connectRemoteMic(remoteMic);
 
-    await page.getByTestId('save-button').click();
-    await page.getByTestId('manage-songs').click();
-    await page.getByTestId('edit-songs').click();
-    await page.locator('[data-test="hide-song"][data-song="zzz-last-polish-1994"]').click();
-    await expect(await page.locator('[data-test="restore-song"][data-song="zzz-last-polish-1994"]')).toBeVisible();
-    await expect(await remoteMic.getByTestId('zzz-last-polish-1994')).not.toBeVisible();
+    await pages.smartphonesConnectionPage.saveAndGoToSing();
+    await pages.mainMenuPage.goToManageSongs();
+    await pages.manageSongsPage.goToEditSongs();
+    await pages.editSongsPage.hideSong(songName, songID);
+    await pages.editSongsPage.expectHideSongNotToBeVisible(songID);
+    await expect(remoteMic.getByTestId(songID)).not.toBeVisible();
   });
 
   await test.step('Song list contains custom songs after connecting', async () => {
-    await page.getByTestId('convert-song').click();
-    await page.getByTestId('input-txt').fill(txtfile);
-    await page.getByTestId('next-button').click();
-    await page.locator('[data-test="video-url"] input').fill(`https://www.youtube.com/watch?v=8YKAHgwLEMg`);
-    await page.getByTestId('next-button').click();
-    await page.getByTestId('next-button').click();
-    await page.getByTestId('save-button').click();
-    await page.getByTestId('share-songs-disagree').click();
-    await expect(await page.getByTestId('convert-song')).toBeVisible();
-
+    await pages.editSongsPage.goToImportUltrastar();
+    await pages.songEditingPage.enterSongTXT(txtfile);
+    await pages.songEditingPage.goNext();
+    await pages.songEditingPage.enterVideoURL(videoURL);
+    await pages.songEditingPage.goNext();
+    await pages.songEditingPage.goNext();
+    await pages.songEditingPage.saveChanges();
+    await pages.editSongsPage.disagreeToShareAddSongs();
+    await pages.editSongsPage.expectSongToBeVisible(convertedSongName, convertedSongID);
     await remoteMic.getByTestId('menu-song-list').click();
-    await expect(await remoteMic.getByTestId('convert-test')).toBeVisible();
+    await expect(remoteMic.getByTestId(convertedSongID)).toBeVisible();
   });
 });
