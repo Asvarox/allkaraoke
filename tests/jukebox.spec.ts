44dd5a4ff262dd769542eb085fdfb3221c0a4e3d
Renata <renbabiuch@gmail.com>
2024-02-21 20:37:15 +0100

    add steps to jukebox-test

--- tests/jukebox.spec.ts
+++ tests/jukebox.spec.ts
@@ -11,16 +11,23 @@ test.beforeEach(async ({ page, context, browser }) => {
 
 test('Jukebox', async ({ page }) => {
   await page.goto('/?e2e-test');
-  await pages.landingPage.enterTheGame();
-  await pages.inputSelectionPage.skipToMainMenu();
-  await pages.mainMenuPage.goToJukebox();
 
-  const previousSong = await pages.jukeboxPage.currentSongName;
-  await pages.jukeboxPage.navigateToSkipSongByKeyboard();
-  await pages.jukeboxPage.expectCurrentSongNotToBe(previousSong!);
+  await test.step('Go to Jukebox', async () => {
+    await pages.landingPage.enterTheGame();
+    await pages.inputSelectionPage.skipToMainMenu();
+    await pages.mainMenuPage.goToJukebox();
+  });
 
-  const expectedSong = await pages.jukeboxPage.currentSongName;
-  await pages.jukeboxPage.navigateToSingSongByKeyboard();
-  await pages.songLanguagesPage.continueAndGoToSongList();
-  await pages.songListPage.expectSelectedSongToBe(expectedSong!);
+  await test.step('Skip the song - the jukebox should display next song', async () => {
+    const previousSong = await pages.jukeboxPage.currentSongName;
+    await pages.jukeboxPage.navigateToSkipSongByKeyboard();
+    await pages.jukeboxPage.expectCurrentSongNotToBe(previousSong!);
+  });
+
+  await test.step('After click to sing a song, that song should appear as a preview in the song list', async () => {
+    const expectedSong = await pages.jukeboxPage.currentSongName;
+    await pages.jukeboxPage.navigateToSingSongByKeyboard();
+    await pages.songLanguagesPage.continueAndGoToSongList();
+    await pages.songListPage.expectSelectedSongToBe(expectedSong!);
+  });
 });
