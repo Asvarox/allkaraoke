3f901cb384f357999dd195d38ba3a5baab391607
Renata <renbabiuch@gmail.com>
2024-03-13 16:04:33 +0100

    change variables to 1 object

--- tests/edit-song.spec.ts
+++ tests/edit-song.spec.ts
@@ -14,14 +14,16 @@ test.beforeEach(async ({ page, context, browser }) => {
 // Not disabling it globally so in case SW breaks the app it is caught by other tests
 test.use({ serviceWorkers: 'block' });
 
-const songID = 'e2e-single-english-1995';
-const expectedURL = 'sourceUrl';
-const expectedAuthorName = 'author';
-const expectedAuthorURL = 'authorUrl';
-const expectedVideoURL = 'https://www.youtube.com/watch?v=W9nZ6u15yis';
-const expectedSongLanguage = 'English';
-const expectedReleaseYear = '1995';
-const expectedSongBPM = '200';
+const editedSong = {
+  songID: 'e2e-single-english-1995',
+  url: 'sourceUrl',
+  authorName: 'author',
+  authorURL: 'authorUrl',
+  videoURL: 'https://www.youtube.com/watch?v=W9nZ6u15yis',
+  language: 'English',
+  releaseYear: '1995',
+  bpm: '200',
+};
 
 test('Edit song', async ({ page }) => {
   await page.goto('/?e2e-test');
@@ -31,18 +33,18 @@ test('Edit song', async ({ page }) => {
     await pages.inputSelectionPage.skipToMainMenu();
     await pages.mainMenuPage.goToManageSongs();
     await pages.manageSongsPage.goToEditSongs();
-    await pages.editSongsPage.editSong(songID);
+    await pages.editSongsPage.editSong(editedSong.songID);
   });
 
   await test.step('Check basic song info', async () => {
-    await expect(pages.songEditBasicInfoPage.urlSourceInput).toHaveValue(expectedURL);
+    await expect(pages.songEditBasicInfoPage.urlSourceInput).toHaveValue(editedSong.url);
   });
 
   await test.step('Check info about author and video', async () => {
     await pages.songEditBasicInfoPage.goToAuthorAndVideoStep();
-    await expect(pages.songEditAuthorAndVideoPage.authorNameInput).toHaveValue(expectedAuthorName);
-    await expect(pages.songEditAuthorAndVideoPage.authorUrlInput).toHaveValue(expectedAuthorURL);
-    await expect(pages.songEditAuthorAndVideoPage.videoUrlInput).toHaveValue(expectedVideoURL);
+    await expect(pages.songEditAuthorAndVideoPage.authorNameInput).toHaveValue(editedSong.authorName);
+    await expect(pages.songEditAuthorAndVideoPage.authorUrlInput).toHaveValue(editedSong.authorURL);
+    await expect(pages.songEditAuthorAndVideoPage.videoUrlInput).toHaveValue(editedSong.videoURL);
   });
 
   await test.step('Go to sync lyrics to video Step', async () => {
@@ -52,8 +54,8 @@ test('Edit song', async ({ page }) => {
 
   await test.step('Check song metadata', async () => {
     await pages.songEditSyncLyricsToVideoPage.goToMetadataStep();
-    await expect(pages.songEditMetadataPage.songLanguageElement).toContainText(expectedSongLanguage);
-    await expect(pages.songEditMetadataPage.releaseYearInput).toHaveValue(expectedReleaseYear);
-    await expect(pages.songEditMetadataPage.bpmSongInput).toHaveValue(expectedSongBPM);
+    await expect(pages.songEditMetadataPage.songLanguageElement).toContainText(editedSong.language);
+    await expect(pages.songEditMetadataPage.releaseYearInput).toHaveValue(editedSong.releaseYear);
+    await expect(pages.songEditMetadataPage.bpmSongInput).toHaveValue(editedSong.bpm);
   });
 });
