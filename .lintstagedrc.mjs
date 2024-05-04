987f1bbfed855880ea12fa16e6b8858395aa92cf
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2024-05-04 09:04:25 +0200

    stop running tests if source code didnt change

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,7 @@
 export default {
   '*.{mjs,ts,tsx}': ['prettier --config ./.prettier.config.ci.js --write', 'eslint --cache --fix'],
   '*.{ts,tsx}': () => 'pnpm tsc --noEmit',
+  'src/**/*.{ts,tsx}': () => ['pnpm run test --watch=false', 'pnpm unimported --no-cache'],
   'public/songs/*.txt': [
     'pnpm ts-node scripts/updateLastUpdate.ts',
     'pnpm generate-index',
