7d08a4480947a02fe814988976305bbc90b781a2
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2024-05-05 09:50:10 +0200

    allow sending feedback about songs

--- src/Scenes/Game/Singing/GameOverlay/Components/PauseMenu.tsx
+++ src/Scenes/Game/Singing/GameOverlay/Components/PauseMenu.tsx
@@ -1,9 +1,12 @@
 import { MenuButton, MenuContainer } from 'Elements/Menu';
 import Modal from 'Elements/Modal';
+import RateSong from 'Scenes/Game/Singing/GameOverlay/Components/RateSong';
+import GameState from 'Scenes/Game/Singing/GameState/GameState';
 import SelectInputModal from 'Scenes/SelectInput/SelectInputModal';
 import InputLag from 'Scenes/Settings/InputLag';
 import useKeyboardNav from 'hooks/useKeyboardNav';
 import { useEffect, useRef, useState } from 'react';
+import isE2E from 'utils/isE2E';
 
 interface Props {
   onResume: () => void;
@@ -23,21 +26,39 @@ export default function PauseMenu({ onResume, onExit, onRestart }: Props) {
 
   const { register } = useKeyboardNav({ enabled: !isInputModalOpen });
 
+  const [rateSongOpen, setRateSongOpen] = useState(false);
+  const handleExit = () => {
+    const progress = GameState.getSongCompletionProgress();
+    // todo add e2e test
+    if (progress < 0.7 && !isE2E()) {
+      setRateSongOpen(true);
+    } else {
+      onExit();
+    }
+  };
+
   return (
     <Modal onClose={onResume}>
-      <MenuContainer>
-        <MenuButton {...register('button-resume-song', onResume)} ref={menuRef}>
-          Resume song
-        </MenuButton>
-        <MenuButton {...register('button-restart-song', onRestart)}>Restart song</MenuButton>
-        <MenuButton {...register('button-exit-song', onExit)}>Exit song</MenuButton>
-        <MenuButton {...register('input-settings', () => setIsInputModalOpen(true))}>Microphones settings</MenuButton>
-        <hr />
-        <InputLag ref={inputLagRef} {...register('input-lag', () => inputLagRef.current?.focus())} />
-      </MenuContainer>
-      {isInputModalOpen && (
-        <SelectInputModal onClose={() => setIsInputModalOpen(false)} closeButtonText={'Back to Pause Menu'} />
+      {!rateSongOpen && (
+        <>
+          <MenuContainer>
+            <MenuButton {...register('button-resume-song', onResume)} ref={menuRef}>
+              Resume song
+            </MenuButton>
+            <MenuButton {...register('button-restart-song', onRestart)}>Restart song</MenuButton>
+            <MenuButton {...register('button-exit-song', handleExit)}>Exit song</MenuButton>
+            <MenuButton {...register('input-settings', () => setIsInputModalOpen(true))}>
+              Microphones settings
+            </MenuButton>
+            <hr />
+            <InputLag ref={inputLagRef} {...register('input-lag', () => inputLagRef.current?.focus())} />
+          </MenuContainer>
+          {isInputModalOpen && (
+            <SelectInputModal onClose={() => setIsInputModalOpen(false)} closeButtonText={'Back to Pause Menu'} />
+          )}
+        </>
       )}
+      {rateSongOpen && <RateSong onExit={onExit} register={register} />}
     </Modal>
   );
 }
---
+++ src/Scenes/Game/Singing/GameOverlay/Components/RateSong.tsx
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled';
+import { CheckBox, CheckBoxOutlineBlank } from '@mui/icons-material';
+import { MenuButton, MenuContainer } from 'Elements/Menu';
+import useKeyboardNav from 'hooks/useKeyboardNav';
+import posthog from 'posthog-js';
+import { useEffect, useRef, useState } from 'react';
+
+interface Props {
+  onExit: () => void;
+  register: ReturnType<typeof useKeyboardNav>['register'];
+}
+
+export default function RateSong({ register, onExit }: Props) {
+  const menuRef = useRef<null | HTMLButtonElement>(null);
+  const [lyricsNotInSync, setLyricsNotInSync] = useState(false);
+  const [volumeWrong, setVolumeWrong] = useState(false);
+
+  useEffect(() => {
+    menuRef.current?.focus();
+  }, [menuRef]);
+
+  const handleRate = () => {
+    if (lyricsNotInSync) {
+      posthog.capture('rate-song', { type: 'not-in-sync' });
+    }
+    if (volumeWrong) {
+      posthog.capture('rate-song', { type: 'wrong-volume' });
+    }
+    onExit();
+  };
+
+  return (
+    <>
+      <MenuContainer>
+        <h1>Is the song OK?</h1>
+        <h4>If there's something wrong with the song, let me know so I can fix it</h4>
+        <Checkbox {...register('button-not-in-sync', () => setLyricsNotInSync((current) => !current))} size={'small'}>
+          <Check>{lyricsNotInSync ? <CheckBox /> : <CheckBoxOutlineBlank />}</Check>
+          <span>Lyrics are not in sync</span>
+        </Checkbox>
+        <Checkbox {...register('button-wrong-volume', () => setVolumeWrong((current) => !current))} size={'small'}>
+          <Check>{volumeWrong ? <CheckBox /> : <CheckBoxOutlineBlank />}</Check>
+          Volume is too high/low
+        </Checkbox>
+        <hr />
+        <MenuButton {...register('button-song-ok', handleRate, undefined, true)} ref={menuRef}>
+          {volumeWrong || lyricsNotInSync ? 'Submit and exit' : 'All good, exit the song'}
+        </MenuButton>
+      </MenuContainer>
+    </>
+  );
+}
+
+// todo make it a generic component
+const Checkbox = styled(MenuButton)`
+  justify-content: flex-start;
+  gap: 1rem;
+`;
+
+const Check = styled.div`
+  svg {
+    width: 3rem;
+    height: 3rem;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
