e6aedd12cb40e405348118d4bcd2751480f2e82a
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2024-10-13 10:47:04 +0200

    test

--- src/modules/Elements/NoPrerender.tsx
+++ src/modules/Elements/NoPrerender.tsx
@@ -2,6 +2,7 @@ import isPreRendering from 'modules/utils/isPreRendering';
 import { PropsWithChildren } from 'react';
 
 export default function NoPrerender(props: PropsWithChildren<{}>) {
+  console.log(props);
   if (isPreRendering) {
     return null;
   }
