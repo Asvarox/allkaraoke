a527332142659411df45fe3a6d7f75d72a2d8b67
Renata <renbabiuch@gmail.com>
2024-10-16 18:39:29 +0200

    remove test (already exist) and rename test file

---
+++ tests/remote-mics-remove-player-mic.spec.ts
@@ -0,0 +1,12 @@
+import { test } from '@playwright/test';
+import initialise from './PageObjects/initialise';
+import { initTestMode, mockSongs } from './helpers';
+
+let pages: ReturnType<typeof initialise>;
+test.beforeEach(async ({ page, context, browser }) => {
+  pages = initialise(page, context, browser);
+  await initTestMode({ page, context });
+  await mockSongs({ page, context });
+});
+
+test('', async ({ page, browser, context }) => {});
