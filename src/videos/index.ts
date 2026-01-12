ca10cf44cb71ecc1cd102f3c3635ec65acf2f43d
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2026-01-12 15:31:20 +0100

    remove remotion

--- src/videos/index.ts
+++
@@ -1,8 +0,0 @@
-import { loadFont } from '@remotion/google-fonts/Roboto';
-import { registerRoot } from 'remotion';
-import '~/index.css';
-import { RemotionRoot } from '~/videos/Root';
-
-loadFont();
-
-registerRoot(RemotionRoot);
--- src/videos/support/AbsoluteFrame/useAbsoluteFrame.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { AbsoluteFrameContext } from '~/videos/support/AbsoluteFrame/Provider';
-
-export default function useAbsoluteFrame() {
-  const context = useContext(AbsoluteFrameContext);
-
-  return context.frame;
-}
