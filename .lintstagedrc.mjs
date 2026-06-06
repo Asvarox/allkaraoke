7d232c19721fc71e4e9acdf3e4bbfa90593bc802
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2026-06-06 09:48:39 +0200

    run tests only against changed files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
   '*.{mjs,ts,tsx}': ['prettier --config ./.prettier.config.ci.js --write', 'eslint --cache --fix'],
   '*.{ts,tsx}': () => 'pnpm type-check',
-  '{src,functions}/**/*.{ts,tsx}': () => ['pnpm run test --watch=false', 'pnpm knip'],
+  '{src,functions}/**/*.{ts,tsx}': () => ['pnpm run test --watch=false --changed --passWithNoTests', 'pnpm knip'],
 };
