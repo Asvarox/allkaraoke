07ec1a04c80aac451db366fcb19032c962dd06b9
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2026-07-12 10:32:46 +0200

    optimize linstaged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,6 @@
 export default {
   '*.{mjs,ts,tsx}': ['prettier --config ./.prettier.config.ci.js --write', 'oxlint --fix'],
   '*.{ts,tsx}': () => 'pnpm type-check',
-  '{src,functions}/**/*.{ts,tsx}': () => ['pnpm run test --watch=false --changed --passWithNoTests', 'pnpm knip'],
+  '{src,functions}/**/*.{ts,tsx}': () => 'pnpm test:staged',
+  '{package.json,knip.json,{src,functions,tests,scripts}/**/*.{ts,tsx,js,mjs}}': () => 'pnpm knip',
 };
