4755a2e40013b1af3c8070166714ea7884b77e6a
Aleksander Tatarczyk <tatarczyk.aleksander@gmail.com>
2026-08-15 11:06:39 +0200

    init design sync

---
+++ .design-sync/entry.tsx
@@ -0,0 +1,18 @@
+export { BottomSheet } from '~/modules/elements/akui/bottom-sheet';
+export type { BottomSheetProps } from '~/modules/elements/akui/bottom-sheet';
+
+export { Button, ButtonLink } from '~/modules/elements/akui/button';
+export type { ButtonSize } from '~/modules/elements/akui/button';
+
+export { Chip } from '~/modules/elements/akui/chip';
+
+export { Icon } from '~/modules/elements/akui/icon';
+export type { IconName, IconProps } from '~/modules/elements/akui/icon';
+
+export { Kbd } from '~/modules/elements/akui/kbd';
+
+export { Menu } from '~/modules/elements/akui/menu.tsx';
+
+export { Selector } from '~/modules/elements/akui/selector';
+
+export { Skeleton } from '~/modules/elements/akui/skeleton';
--- src/modules/utils/is-e2-e.ts
+++ src/modules/utils/is-e2-e.ts
@@ -1,12 +1,12 @@
-const wasE2E = global?.sessionStorage?.getItem('isE2E');
-const isE2EParamPresent = global?.location?.search.includes('e2e-test');
+const wasE2E = globalThis?.sessionStorage?.getItem('isE2E');
+const isE2EParamPresent = globalThis?.location?.search.includes('e2e-test');
 export default function isE2E() {
   if (wasE2E) {
     return true;
   }
-  const isE2E = global?.isE2ETests || isE2EParamPresent;
+  const isE2E = globalThis?.isE2ETests || isE2EParamPresent;
   if (isE2E) {
-    global?.sessionStorage?.setItem('isE2E', 'true');
+    globalThis?.sessionStorage?.setItem('isE2E', 'true');
   }
   return isE2E;
 }
