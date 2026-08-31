436736adccc15b32a60beaa76d29b6b293fac51b
Asvarox <tatarczyk.aleksander@gmail.com>
2026-08-31 02:50:44 +0000

    Deploying to gh-pages from @ Asvarox/allkaraoke@631a02d583f21721ddaef7573e0792049dcf39e9 🚀

--- allkaraoke_party/index.mjs
+++ allkaraoke_party/index.mjs
@@ -1,7 +1,7 @@
 (function() {
 	try {
 		var e = "undefined" != typeof window ? window : "undefined" != typeof global ? global : "undefined" != typeof globalThis ? globalThis : "undefined" != typeof self ? self : {};
-		e.SENTRY_RELEASE = { id: "4ede8b166100ffd43333edabc3b290aef82e9aca" };
+		e.SENTRY_RELEASE = { id: "631a02d583f21721ddaef7573e0792049dcf39e9" };
 		e._sentryModuleMetadata = e._sentryModuleMetadata || {}, e._sentryModuleMetadata[new e.Error().stack] = function(e) {
 			for (var n = 1; n < arguments.length; n++) {
 				var a = arguments[n];
